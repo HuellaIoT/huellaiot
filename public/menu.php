f3d86e05be5ca18e955bcf8375325ef23da3f940
HuellaIoT <huellaiot@gmail.com>
2025-07-09 02:42:10 -0500

    Update menu.php

--- public/menu.php
+++ public/menu.php
@@ -6,11 +6,18 @@ if (!isset($_SESSION["logueado"])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head><title>Menú</title></head>
-<body>
-<h2>Menú Principal</h2>
-<a href="agregar_huella.php"><button>Agregar Pacientes</button></a><br><br>
-<a href="ver_pacientes.php"><button>Ver Pacientes</button></a>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Menú Principal</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light text-center pt-5">
+  <h2 class="mb-4">Sistema de Huellas</h2>
+  <div class="d-grid gap-3 col-4 mx-auto">
+    <a href="agregar_huella.php" class="btn btn-success btn-lg">Agregar Paciente</a>
+    <a href="ver_pacientes.php" class="btn btn-primary btn-lg">Ver Pacientes</a>
+  </div>
 </body>
 </html>
+
